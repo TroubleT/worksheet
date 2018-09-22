49b37af81a2330bed14b143dc50d656ee0931c74
Hryhoriy Starykov <starykovhryhoriy@gmail.com>
2018-09-22 06:41:45 +0000

    made HTML doc

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// const topBtn = document.getElementById("topBtn")
+// .addEventListener('click', function() {
+//       const modal = document.getElementById('modal');
+//       modal.style.display = "block";
+//     });
+const topBtn = document.getElementById("topBtn")
+.addEventListener('click', function() {
+      alert("Example work of function");
+    });
